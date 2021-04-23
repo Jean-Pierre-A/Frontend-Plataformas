42c5da3123e4ff6f9bdda82dee3bc1c7fc78c746
Jean-Pierre-A <jean.agudelo@upb.edu.co>
2021-04-22 23:33:51 -0500

    Navegancion centralizada 4

--- js/main.js
+++ js/main.js
@@ -1,11 +1,4 @@
-var sesiones = [];
 
-window.onload = init;
-function init(){
-	console.log("ENTRA A INIT");
-	asignarVariables();
-	asignarEventos();
-}
 $(document).ready(function () {
   
 
@@ -40,26 +33,4 @@ $(document).ready(function () {
   $("#demo11").pogoSlider(transitionDemoOpts);
   $("#demo12").pogoSlider(transitionDemoOpts);
 });
-function asignarVariables() {
-	console.log("ENTRA A asignar variables");
-  sesiones["about.html"] = document.getElementById("about_road");
-  sesiones["news.html"] = document.getElementById("news_road");
-  sesiones["schedule.html"] = document.getElementById("schedule_road");
-  sesiones["gallery.html"] = document.getElementById("gallery_road");
-  sesiones["drivers.html"] = document.getElementById("drivers_road");
-}
-function asignarEventos(){
-	console.log("ENTRA A asignar eventos");
-	var temp;
-	for(var i in sesiones){
-		temp = sesiones[i];
-		temp.addEventListener("click",navegacion);
-	}
-}
-function navegacion(event){
-	console.log("ENTRA A navegacion");
-	var id = event.target.id;
-	location.href= id;
-	
-}
 
